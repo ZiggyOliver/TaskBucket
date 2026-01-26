c9c494571730a280135e203011895a4be6dd6c6d
Laptop <zobiamiwe@gmail.com>
2026-01-26 17:37:57 +0000

    half completed AddTaskBucket()

---
+++ AddTaskBucket.py
@@ -0,0 +1,22 @@
+import sqlite3
+
+class TaskBucket:
+    task = float("nan")
+    bucket = float("nan")
+    sessionTimeStart = float("nan")
+    sessionTimeEnd = float("nan")
+
+    def __init__(task, bucket, sessionTimeStart, sessionTimeEnd):
+        self.task = task
+        self.bucket = bucket
+        self.sessionTimeStart = sessionTimeStart
+        self.sessionTimeEnd = sessionTimeEnd
+
+
+def AddTaskBucketToDB(taskBucket):
+    connection = sqlite3.connect("TaskBucket_Data.db")
+    cursor = connection.cursor
+
+    cursor.execute(f"""
+    INSERT INTO TaskBuckets (taskID, bucketID, sessionTimeStart, SessionTimeEnd)
+    VALUES ({taskBucket.task}, {taskBucket.bucket}, taskBucket.sessionTimeStart, taskBucket.sessionTimeEnd
