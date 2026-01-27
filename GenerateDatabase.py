257c3ebafb99cb77089e6866c4ee369954a6c347
Laptop <zobiamiwe@gmail.com>
2026-01-27 20:12:31 +0000

    TAA 002

--- GenerateDatabase.py
+++ GenerateDatabase.py
@@ -34,5 +34,4 @@ def GenerateDatabase():
     sessionTimeEnd INTEGER)
     """)
 
-    
-]
+
--- TaskArrangementAlgorithm.py
+++ TaskArrangementAlgorithm.py
@@ -1,21 +1,35 @@
+# Task Arrangement Algorithm AKA please-could-you-spare-a-bucket-for-my-poor-little-task-sir-He's-so-very-cold-and-hungry-sir-He-just-needs-a-bucket-sir
+
 import shutil
 import sqlite3
 import epoch
 import os
+import AddBucket
+import
 
-shutil.copyfile("./TaskBucket_Data.db","./TaskBucket_Data_copy.db")
+shutil.copyfile("TaskBucket_Data.db","TaskBucket_Data_copy.db")
 
-connection = sqlite3.connect("TaskBucket_Data")
+connection = sqlite3.connect("TaskBucket_Data.db")
 cursor = connection.cursor()
 
 #finding next buckets
-print(cursor.execute(f"""
-SELECT bucketID
+cursor.execute(f"""
+SELECT bucketType, startTime, finishTime
 FROM Buckets
-WHERE sessionTimeStart > {epoch.now()}
+WHERE startTime > {epoch.now()}
 
-"""))
+""")
+
+bucketsAfterCurrentTime = []
+
+for fetchedBucket in cursor.fetchall():
+    createdBucket = AddBucket.Bucket(fetchedBucket[0],fetchedBucket[1],fetchedBucket[2])
+    bucketsAfterCurrentTime.append(createdBucket)
+
+#main loop
+for bucket in BucketsAfterCurrentTime:
+    if bucket
 
 
-os.remove("./TaskBucket_Data_copy")
-
+    
+os.remove("TaskBucket_Data_copy.db")
